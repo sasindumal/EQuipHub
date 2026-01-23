b387d09b914467127ac18d3d7ac03e30d0991b36
Ravindu56 <raveest56@gmail.com>
2026-01-24 05:23:21 +0530

    Backend Initialization

---
+++ backend/api/src/main/java/com/equiphub/api/EquipHubApplication.java
@@ -0,0 +1,13 @@
+package com.equiphub.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquipHubApplication.class, args);
+	}
+
+}
---
+++ backend/api/src/test/java/com/equiphub/api/EquipHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equiphub.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
